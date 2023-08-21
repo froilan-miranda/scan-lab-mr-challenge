6f1b4e44c549fe5771a886effefd48ab17021567
vaquero <froilan@virtual-artifact.com>
2023-08-21 14:59:12 +0100

    set up empty scene and camera

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
-import { ref } from 'vue'
-import * as THREE from 'three'
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import * as THREE from 'three';
 
 const webGl = ref();
 
+onMounted(() => {
+  // scene setup
+  console.log("component has mounted")
+  const scene = new THREE.Scene();
+  scene.backgroundColor = 0xffffff;
+  scene.fog = new THREE.Fog(0xffffff, 0.0025, 50);
+
+  // setup camera and basic renderer
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight*0.66), 0.1, 1000);
+  camera.position.x = -3;
+  camera.position.z = 8;
+  camera.position.y = 2;
+
+});
 </script>
 
 <template>
